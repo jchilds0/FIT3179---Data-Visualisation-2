04e7da3121ad5c9b32d49e0d2dba67b4d464adf7
JWonderchilds <joshua.childs01@outlook.com>
2022-09-19 21:44:53 +1000

    maps

---
+++ maps_prep/adelie_penguins_telemetry_cleaning.py
@@ -0,0 +1,15 @@
+import csv
+
+def format_csv(file):
+    csv_reader = csv.reader(file)
+    lst = [row for row in csv_reader]
+    return [[row[0], row[1], row[2], row[3], row[4], row[5], float(row[6]), float(row[7])] for row in lst[1:]]
+
+def split_into_penguins():
+    with open("data/adelie_penguin_telemetry.csv", "r") as file:
+        adelie_penguin_csv = format_csv(file)
+        print(adelie_penguin_csv[0])
+        print(adelie_penguin_csv[1])
+
+if __name__ == "__main__":
+    split_into_penguins()
